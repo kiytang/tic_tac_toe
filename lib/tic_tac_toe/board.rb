7459d6bf23fa4103789cd0763395f7bc50981d4e
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-24 16:26:26 +0100

    Rearranged cordinate systme fro (y,x) to (x,y)

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -4,6 +4,14 @@ module TicTacToe
     def initialize(input = {})
       @grid = input.fetch(:grid, default_grid)  
     end
+
+    def get_cell(x,y)
+      grid[y][x]
+    end
+
+    def set_cell(x,y, value)
+      get_cell(x,y).value = value
+    end
   
     private
    
