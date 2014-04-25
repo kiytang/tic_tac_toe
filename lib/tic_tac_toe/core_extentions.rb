81f4c1f81162ffab04202462758951c5c6ad8e71
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-25 11:53:54 +0100

    Added RSpec to test the any_empty? method

--- lib/tic_tac_toe/core_extentions.rb
+++ lib/tic_tac_toe/core_extentions.rb
@@ -2,4 +2,12 @@ class Array
 	def all_empty?
 		self.all? {|element| element.to_s.empty?}
 	end
+
+  def all_same?
+    self.all? {|element| element == self[0]}
+  end
+
+  def any_empty?
+    self.any? {|element| element.to_s.empty?}  
+  end
 end
--- spec/core_extentions_spec.rb
+++ spec/core_extentions_spec.rb
@@ -14,4 +14,48 @@ describe Array do
       expect([].all_empty?).to be_true
     end
   end
+
+  context "#all_same?" do
+    it "returns true if all elements of the Array are the same" do
+      expect(["X","X","X"].all_same?).to be_true
+    end
+    
+    it "returns false if some of the Array elements are different" do
+      expect(["","X",""].all_same?).to be_false
+    end
+
+    it "returns ture for an empty Array" do
+      expect([].all_same?).to be_true
+    end
+  end
+
+  context "#any_empty?" do
+    it "returns true if any one of the element of the Array is empty" do
+      expect(["","X", "X"].any_empty?).to be_true
+    end
+
+    it "returns false if no elements are empty" do
+      expect(["X","X","X"].any_empty?).to be_false
+    end
+
+    it "returns false for an empty Array" do
+      expect([].any_empty?).to be_false
+    end
+  end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
