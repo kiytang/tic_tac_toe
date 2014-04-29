ca5b65cb2c3706a6cbc89d0ecf95d97f1d3997fc
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-29 19:35:58 +0100

    Tests for random selection of  a current_player

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+module TicTacToe
+  describe Game do
+
+    let (:bob)   {Player.new({color: "X", name: "bob"})}
+    let (:frank) {Player.new({color: "O", name: "frank"})}
+
+    context "#initialize" do
+      it "randomly selects a current_player" do
+        Array.any_instance.stub(:shuffle) {[frank, bob]}
+        game = Game.new([bob, frank])
+        expect(game.current_player).to eq frank
+      end
+      
+    end
+end
