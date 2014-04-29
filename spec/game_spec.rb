62a3b528eb717442ac7a21d14853efe91b787da3
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-29 19:57:47 +0100

    Added tests for class swich_players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,10 +12,26 @@ module TicTacToe
       end
 
       it "randomly selects another player" do
-        Array.any_instance.stub(:shuffle) {[Frank, bob]}
+        Array.any_instance.stub(:shuffle) {[frank, bob]}
         game = Game.new([bob, frank])
         expect(game.other_player).to eq bob  
       end
+    end
+
+    context "#switch_players" do
+      it "will set @current_player to @other_player" do
+        game = Game.new([bob, frank])
+        other_player = game.other_player
+        game.switch_players
+        expect(game.current_player).to eq other_player  
+      end
+
+      it "will set @other_player to @current_player" do
+        game- Game.new([bob, frank])
+        current_player = game.current_player
+        game.switch_players
+        expect(game.other_player).to eq current_player
+      end 
     end
   end
 end
