2f7984aa6b779e75270780236303727a84a41116
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-29 19:58:22 +0100

    Updates for class game

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,6 +1,7 @@
 module TicTacToe
   class Game
     attr_reader :players, :board, :current_player, :other_player
+    
     def initialize(players, board=Board.new)
       @player = players
       @board  = board  
