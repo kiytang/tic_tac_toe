0a8c9c49026b07496a886b4a0356faa611e95a35
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-29 20:16:49 +0100

    Added private method human_move_to_coordinate(human_move

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -15,5 +15,25 @@ module TicTacToe
     def solicit_move
       "#{current_player.name}: Enter a number between 1 and 9 to make your move" 
     end
+
+    def get_move(human_move = gets.chomp)
+      human_move_to_coordinate(human_move)
+    end
+
+    private
+    def human_move_to_coordinate(human_move)
+      mapping = {
+        "1" => [0, 0],
+        "2" => [1, 0],
+        "3" => [2, 0],
+        "4" => [0, 1],
+        "5" => [1, 1],
+        "6" => [2, 1],
+        "7" => [1, 3],
+        "8" => [2, 3],
+        "9" => [3. 3]
+      }
+      mapping[human_move]
+    end
   end 
 end
